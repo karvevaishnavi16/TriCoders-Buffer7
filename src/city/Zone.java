d6207298d96f2abc796a218488ebf769f80b0dc7
karvevaishnavi16 <karvevaishanavi16@gmail.com>
2026-03-29 23:57:52 +0530

    Write code for zone.java

--- src/city/Zone.java
+++ src/city/Zone.java
@@ -1,4 +1,30 @@
-package city;
 public class Zone {
-    
+    int zoneId;
+    String zoneName;
+    double waterLevel;
+    int sosCount;
+    double infraStress;
+    double riskScore;
+    boolean isCritical;
+
+    Zone(int zoneId, String zoneName) {
+        this.zoneId = zoneId;
+        this.zoneName = zoneName;
+        this.waterLevel = 0.0;
+        this.sosCount = 0;
+        this.infraStress = 1.0;
+        this.riskScore = 0.0;
+        this.isCritical = false;
+    }
+
+    void printStatus() {
+        System.out.println("-----------------------------");
+        System.out.println("Zone        : " + zoneName);
+        System.out.println("Water Level : " + waterLevel + " m");
+        System.out.println("SOS Count   : " + sosCount + " reports");
+        System.out.println("Infra Stress: " + infraStress + "x normal");
+        System.out.println("Risk Score  : " + riskScore);
+        System.out.println("Status      : " + (isCritical ? "CRITICAL" : "NORMAL"));
+        System.out.println("-----------------------------");
+    }
 }
